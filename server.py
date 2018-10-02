9f93abc26b28201712f72511789b515b449977ab
fa2018 <kywirieck@gmail.com>
2018-10-02 09:26:18 +0200

    receive method

--- server.py
+++ server.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request
+from flask import Flask, request, redirect, url_for, send_from_directory
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = '/images/'
+ALLOWED_EXTENSIONS = set(['jpg'])
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route("/")
 def hello():
@@ -32,9 +37,36 @@ def calc_pattern():
     return response[:-1] + ']'
 
 
+@app.route("/receive", methods=['POST'])
+def receive():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join[app.config['UPLOAD_FOLDER'], filename])
+            return redirect(url_for('uploaded_file'), filename=filename)
+
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    ''' 
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+
 if __name__ == "__main__":
     
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
 
 
 
